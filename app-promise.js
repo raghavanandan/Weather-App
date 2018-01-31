1ac31637c37db0e9b56028427d4477bc5a2d25f7
Raghav <raghavendrakumar.anandan@sjsu.edu>
2018-01-30 23:25:03 -0800

    Weather App with promises

--- app-promise.js
+++ app-promise.js
@@ -24,16 +24,16 @@ axios.get(geocodeURL).then((response) => {
   if(response.data.status === 'ZERO_RESULTS') {
     throw new Error('Unable to find that address');         // Throwing to catch block
   }
-  
+
   var lat = response.data.results[0].geometry.location.lat;
   var lng = response.data.results[0].geometry.location.lng;
-  var weatherURL = `https://api.darksky.net/forecast/aace21cfc1b53b495d767a9c4d44eafa/${lat},${lng}`;
+  var weatherURL = `https://api.darksky.net/forecast/aace21cfc1b53b495d767a9c4d44eafa/${lat},${lng}?units=si`;
   console.log(response.data.results[0].formatted_address);
   return axios.get(weatherURL);
 }).then((response) => {
   var temperature = response.data.currently.temperature;
   var apparentTemperature = response.data.currently.apparentTemperature;
-  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+  console.log(`It's currently ${temperature}.C but feels like ${apparentTemperature}.C`);
 }).catch((errorMsg) => {
   if(errorMsg.code === 'ENOTFOUND') {
     console.log('Unable to connect to API servers');
